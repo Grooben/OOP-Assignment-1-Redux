afa773bedcddbcfdc94f6b22ee72cfa237b1dfaf
OhGrooben <43583016+OhGrooben@users.noreply.github.com>
2018-12-07 15:35:45 +0000

    Added some initial code to initialise some vectors

---
+++ CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally.cpp
@@ -0,0 +1,14 @@
+// CMP2090M-Nearest-Wally.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "CMP2090M, Oliver Grooby, GRO16605155\n";
+
+	std::vector<std::vector<double>> WallyVector; //This is a temporary Vector, and should be implemented as a Class/Obj soon
+	std::vector<std::vector<double>> SceneVector; //This is a temporary Vector, and should be implemented as a Class/Obj soon
+}

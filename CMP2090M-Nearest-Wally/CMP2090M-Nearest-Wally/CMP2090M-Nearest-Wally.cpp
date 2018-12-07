630fc94a60f5bb83f200820da62fcfad33500c0b
OhGrooben <43583016+OhGrooben@users.noreply.github.com>
2018-12-07 16:31:39 +0000

    Basic IO done!

--- CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally.cpp
+++ CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally.cpp
@@ -1,16 +1,30 @@
 // CMP2090M-Nearest-Wally.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
+#include "IOFunctions.h"
 #include <iostream>
 #include <vector>
+#include <thread>
+
+int threadWrapper(const char *fileName)
+{
+	IO io;
+	int rows = io.countRows(fileName);
+	int cols = io.countCols(fileName);
+	io.read_text(fileName, rows, cols);
+	return 0;
+}
 
 int main()
 {
     std::cout << "CMP2090M, Oliver Grooby, GRO16605155\n";
 
-	std::vector<std::vector<double>> WallyVector; //This is a temporary Vector, and should be implemented as a Class/Obj soon
-	std::vector<std::vector<double>> SceneVector; //This is a temporary Vector, and should be implemented as a Class/Obj soon
+	const char *scenePath = "Cluttered_scene.txt";
+	const char *wallyPath = "Wally_grey.txt";
 
+	std::thread SceneGeneration(threadWrapper, scenePath);
+	std::thread WallyGeneration(threadWrapper, wallyPath);
 
+	SceneGeneration.join();
+	WallyGeneration.join();
 }
--- CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally/IOFunctions.h
+++ CMP2090M-Nearest-Wally/CMP2090M-Nearest-Wally/IOFunctions.h
@@ -1,75 +1,105 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <streambuf>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
 
 // Reads .txt file representing an image of R rows and C Columns stored in filename 
 // and converts it to a 1D array of doubles of size R*C
 // Memory allocation is performed inside readTXT
 // Read .txt file with image of size (R rows x C columns), and convert to an array of doubles
-double* read_text(char *fileName, int sizeR, int sizeC)
+
+class IO 
 {
-	double* data = new double[sizeR*sizeC];
-	int i = 0;
-	ifstream myfile(fileName);
-	if (myfile.is_open())
+public:
+	int countRows(const char *fileName)
+	{
+		int rowCount = 0;
+		std::ifstream TxtFile(fileName);
+		rowCount = std::count(std::istreambuf_iterator<char>(TxtFile), std::istreambuf_iterator<char>(), '\n');
+		return rowCount;
+	}
+
+	int countCols(const char *fileName)
+	{
+		int colCount = 0;
+		std::ifstream TxtFile(fileName);
+		std::string tmp;
+		std::getline(TxtFile, tmp);
+		colCount = (std::count(tmp.begin(), tmp.end(), '\t') + std::count(tmp.begin(), tmp.end(), ' ') + 1) / 2;
+		return colCount;
+	}
+
+	double* read_text(const char *fileName, int sizeR, int sizeC)
+	{
+		double* data = new double[sizeR*sizeC];
+		int i = 0;
+		std::ifstream myfile(fileName);
+		if (myfile.is_open())
+		{
+
+			while (myfile.good())
+			{
+				if (i > sizeR*sizeC - 1) break;
+				myfile >> *(data + i);
+				// cout << *(data+i) << ' '; // This line display the converted data on the screen, you may comment it out. 
+				i++;
+			}
+			myfile.close();
+		}
+
+		else std::cout << "Unable to open file";
+		//cout << i;
+
+		return data;
+	}
+
+
+
+	// Converts a 1D array of doubles of size R*C to .pgm image of R rows and C Columns 
+	// and stores .pgm in filename
+	// Use Q = 255 for greyscale images and 1 for binary images.
+	void write_pgm(char *filename, double *data, int sizeR, int sizeC, int Q)
 	{
 
-		while (myfile.good())
-		{
-			if (i > sizeR*sizeC - 1) break;
-			myfile >> *(data + i);
-			// cout << *(data+i) << ' '; // This line display the converted data on the screen, you may comment it out. 
-			i++;
+		int i, j;
+		unsigned char *image;
+		std::ofstream myfile;
+
+		image = (unsigned char *) new unsigned char[sizeR*sizeC];
+
+		// convert the integer values to unsigned char
+
+		for (i = 0; i < sizeR*sizeC; i++)
+			image[i] = (unsigned char)data[i];
+
+		myfile.open(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+
+		if (!myfile) {
+			std::cout << "Can't open file: " << filename << std::endl;
+			exit(1);
 		}
+
+		myfile << "P5" << std::endl;
+		myfile << sizeC << " " << sizeR << std::endl;
+		myfile << Q << std::endl;
+
+		myfile.write(reinterpret_cast<char *>(image), (sizeR*sizeC) * sizeof(unsigned char));
+
+		if (myfile.fail()) {
+			std::cout << "Can't write image " << filename << std::endl;
+			exit(0);
+		}
+
 		myfile.close();
+
+		delete[] image;
+
 	}
+};
 
-	else std::cout << "Unable to open file";
-	//cout << i;
-
-	return data;
-}
-
-
-
-// Converts a 1D array of doubles of size R*C to .pgm image of R rows and C Columns 
-// and stores .pgm in filename
-// Use Q = 255 for greyscale images and 1 for binary images.
-void write_pgm(char *filename, double *data, int sizeR, int sizeC, int Q)
-{
-
-	int i, j;
-	unsigned char *image;
-	ofstream myfile;
-
-	image = (unsigned char *) new unsigned char[sizeR*sizeC];
-
-	// convert the integer values to unsigned char
-
-	for (i = 0; i < sizeR*sizeC; i++)
-		image[i] = (unsigned char)data[i];
-
-	myfile.open(filename, ios::out | ios::binary | ios::trunc);
-
-	if (!myfile) {
-		cout << "Can't open file: " << filename << endl;
-		exit(1);
-	}
-
-	myfile << "P5" << endl;
-	myfile << sizeC << " " << sizeR << endl;
-	myfile << Q << endl;
-
-	myfile.write(reinterpret_cast<char *>(image), (sizeR*sizeC) * sizeof(unsigned char));
-
-	if (myfile.fail()) {
-		cout << "Can't write image " << filename << endl;
-		exit(0);
-	}
-
-	myfile.close();
-
-	delete[] image;
-
-}
